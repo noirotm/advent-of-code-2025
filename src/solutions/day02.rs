1535ebe2bc3ab1437850834e7f2bc1f62265317d
Marc Noirot <marc.noirot@gmail.com>
2025-12-05 23:57:04 +0100

    [day 02] Switch to math-based solution

--- src/solutions/day02.rs
+++ src/solutions/day02.rs
@@ -35,7 +35,7 @@ impl Solver for Problem {
 
         for r in input {
             for n in r.start..=r.end {
-                if is_id_really_invalid(n) {
+                if is_id_really_invalid2(n) {
                     total += n;
                 }
             }
@@ -90,6 +90,57 @@ fn is_id_really_invalid(id: u64) -> bool {
         })
 }
 
+fn is_id_really_invalid2(id: u64) -> bool {
+    /*
+    111111 / 10^5 + 10^4 + 10^3 + 10^2 + 10^1 + 10^0 = 1
+    111111 % 10^1 = 1
+
+    121212 / 10^4 + 10^2 + 10^0 = 12
+    121212 % 10^2 = 12
+
+    123123 / 10^3 + 10^0 = 123 rem = 0
+    123123 % 10^3 = 123
+    */
+
+    let digits = id.ilog10();
+    for n in 1..=digits.div_ceil(2) {
+        // compute divisor
+        let divisor = (0..=digits)
+            .step_by(n as usize)
+            .take_while(|&v| v <= digits)
+            .map(|v| 10u64.pow(v))
+            .sum::<u64>();
+
+        // divide and get remainder
+        let (q, r) = (id / divisor, id % divisor);
+
+        // if remainder not zero, not divisible, so not repeated
+        if r != 0 {
+            continue;
+        }
+
+        // isolate N last digits which should be equal to quotient if invalid
+        let last = id % 10u64.pow(n);
+
+        // make sure we have the right number of digits
+        /*
+        expecting 2 digits
+        10101 / 10^4 + 10^2 + 10^0 = 1
+        10101 % 10^2 = 1
+        only got 1
+         */
+        if (last.ilog10() + 1) != n {
+            continue;
+        }
+
+        if last == q {
+            return true;
+        }
+    }
+
+    false
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -116,5 +167,20 @@ mod tests {
         assert!(!is_id_really_invalid(1));
         assert!(!is_id_really_invalid(12));
         assert!(is_id_really_invalid(122122));
+    }
+
+    #[test]
+    fn test_is_id_really_invalid2() {
+        assert!(is_id_really_invalid2(111111));
+        assert!(is_id_really_invalid2(1111111));
+        assert!(is_id_really_invalid2(121212));
+        assert!(is_id_really_invalid2(123123123));
+        assert!(!is_id_really_invalid2(1231231234));
+        assert!(is_id_really_invalid2(1111));
+        assert!(!is_id_really_invalid2(1211));
+        assert!(!is_id_really_invalid2(1));
+        assert!(!is_id_really_invalid2(12));
+        assert!(is_id_really_invalid2(122122));
+        assert!(!is_id_really_invalid2(10101));
     }
 }
